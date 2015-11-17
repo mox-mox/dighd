2ba0aa3ef69df4c682bc8964caabca31cb814aa6
hwsimu03 <hwsimu03@creek06.ziti.uni-heidelberg.de>
2015-11-17 17:50:44 +0100

    Well, who would have thought it was so difficult to get this thing not only to work, but also get the test running properly.

--- ex3/lfsr.v
+++ ex3/lfsr.v
@@ -6,16 +6,17 @@ module lfsr #(parameter width=32, parameter [width-1:0] polynomial={width{1'b0}}
                                                                  output wire d_out
                                                                 );
 	reg[width-1:0] data;
-	assign d_out = data[width-1];
+	assign d_out = data[0];
 
 	integer i;
 	always @(posedge clk or negedge res_n) begin
+		//$display ("lfsr data: %B", data[width-1:0]);
 		if(res_n == 1'b0 || clear == 1'b1) begin
-			data<={{width-1{1'b0}},1'b1};
+			data<={1'b1,{width-1{1'b0}}};
 		end else if (enable == 1'b1) begin
-			data[0] <= d_out;
+			data[0] <= data[width-1];
 			for(i=1; i!=width; i = i+1) begin
-				data[i] <= polynomial[i-1] ? data[i-1] ^ d_out : data[i-1];
+				data[i] <= polynomial[i] ? data[i-1]^data[width-1] : data[i-1];
 			end
 		end
 	end
--- ex3/lfsr_tb.v
+++ ex3/lfsr_tb.v
@@ -3,36 +3,69 @@
 
 module lfsr_tb();
 
-	parameter TESTWIDTH = 5;
+	parameter TESTSTEPS = 100;
+	parameter COUNTERWIDTH = 32; // Must be at least log2(TESTSTEPS)
 
 	wire clock;
 	wire shifter_data;
 	reg reset_n;
 	reg cnt_en;
+	reg shift_en;
 	reg cnt_clear;
+	wire [COUNTERWIDTH-1:0] simulation_step;
+	reg one;
 
+
+	// Generated from c++ file in same folder.
+	// Test vector for the long test.
+	reg[TESTSTEPS-1:0] shifter_nominal_output = 100'b1000101100000000101001011000111000001010011000100001101001001001000001000001100000001001000000000001;
+
+	// Test vector for the 10-bit example
+	//reg[TESTSTEPS-1:0] shifter_nominal_output = 101'b0000100001000000000100001000000000100001000000000100001000000000100001000000000100001000000000100001;
+	reg shifter_nominal;
 
 	clk_out clock_I(.clk(clock));
 
-	lfsr #(.width(TESTWIDTH), .polynomial(01010)) lfsr_I(
-	                                                               .clk(clock),
-	                                                               .res_n(reset_n),
-	                                                               .enable(cnt_en),
-	                                                               .clear(cnt_clear),
-	                                                               .d_out(shifter_data)
-	                                                              );
+	counter #(.width(COUNTERWIDTH)) counter_I(
+			                       .clk(clock),
+			                       .res_n(reset_n),
+			                       .enable(cnt_en),
+			                       .clear(cnt_clear),
+			                       .cnt_out(simulation_step)
+			                      );
+	//lfsr #(.width(57), .polynomial(57'b000000000010000000000000000000000100000001001000000000000)) lfsr_I(
+	lfsr #(.width(57), .polynomial(57'b0000000000001001000000010000000000000000000000100000000000)) lfsr_I(
+	//lfsr #(.width(10), .polynomial(10'b0000100000)) lfsr_I( // 10-bit example
+	                                                     .clk(clock),
+	                                                     .res_n(reset_n),
+	                                                     .enable(shift_en),
+	                                                     .clear(cnt_clear),
+	                                                     .d_out(shifter_data)
+	                                                    );
+
 	initial begin
 		reset_n   <= 0;
 		cnt_clear <= 0;
 		cnt_en    <= 0;
+		shift_en  <= 0;
 		#10
-		reset_n <= 1;
-		cnt_en  <= 1;
-
-
-
-
+		$display("Starting Simulation");
+		reset_n   <= 1;
+		shift_en  <= 1;
+		#10
+		cnt_en    <= 1;
 
 		#1000 $stop;
 	end
+
+	always @(posedge clock)
+	begin
+		shifter_nominal = shifter_nominal_output[simulation_step]; // Yes, the blocking assingment is intended here.
+		one = !shifter_data^shifter_nominal;                      // And here, too.
+		$display ("Time: %3d, Soll: %b, Ist: %b, One: %b", simulation_step[31:0], shifter_nominal, shifter_data, one);
+
+		//assert(one);
+	end
+
+
 endmodule
