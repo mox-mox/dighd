594e390a56c65a5b2881be0c757550b42eeeca6c
hwsimu03 <hwsimu03@creek06.ziti.uni-heidelberg.de>
2015-11-17 04:48:09 +0100

    bugfix

--- ex3/lfsr.v
+++ ex3/lfsr.v
@@ -1,4 +1,4 @@
-module lfsr #(parameter width=32, parameter [width-1:0] polynom={width{1'b0}}) (
+module lfsr #(parameter width=32, parameter [width-1:0] polynomial={width{1'b0}}) (
 	                                                             input wire clk,
                                                                  input wire res_n,
                                                                  input wire enable,
@@ -11,11 +11,11 @@ module lfsr #(parameter width=32, parameter [width-1:0] polynom={width{1'b0}}) (
 	integer i;
 	always @(posedge clk or negedge res_n) begin
 		if(res_n == 1'b0 || clear == 1'b1) begin
-			data<={width{1'b0}};
+			data<={{width-1{1'b0}},1'b1};
 		end else if (enable == 1'b1) begin
 			data[0] <= d_out;
-			for(i=1; i==width-1; i = i+1) begin
-				data[i] <= polynom[i-1] ? data[i-1] ^ d_out : data[i-1];
+			for(i=1; i!=width; i = i+1) begin
+				data[i] <= polynomial[i-1] ? data[i-1] ^ d_out : data[i-1];
 			end
 		end
 	end
--- ex3/lfsr_tb.v
+++ ex3/lfsr_tb.v
@@ -1,32 +1,38 @@
 // Testbench for the counter
 
 
-module counter_tb();
+module lfsr_tb();
 
-	parameter TESTWIDTH = 4;
+	parameter TESTWIDTH = 5;
 
 	wire clock;
-	wire [TESTWIDTH-1:0] count;
+	wire shifter_data;
 	reg reset_n;
 	reg cnt_en;
 	reg cnt_clear;
-	
-	
+
+
 	clk_out clock_I(.clk(clock));
 
-	counter #(.width(TESTWIDTH)) counter_I(
-			                       .clk(clock),
-			                       .res_n(reset_n),
-			                       .enable(cnt_en),
-			                       .clear(cnt_clear),
-			                       .cnt_out(count)
-			                      );
+	lfsr #(.width(TESTWIDTH), .polynomial(01010)) lfsr_I(
+	                                                               .clk(clock),
+	                                                               .res_n(reset_n),
+	                                                               .enable(cnt_en),
+	                                                               .clear(cnt_clear),
+	                                                               .d_out(shifter_data)
+	                                                              );
 	initial begin
-		reset_n <= 0;
-		//clock <= 1'b0;
-		cnt_en <= 1;
+		reset_n   <= 0;
 		cnt_clear <= 0;
-		#100
-		reset_n = 1;
+		cnt_en    <= 0;
+		#10
+		reset_n <= 1;
+		cnt_en  <= 1;
+
+
+
+
+
+		#1000 $stop;
 	end
 endmodule
